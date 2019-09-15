f27ac661f70aee14d7f4fc4486532a50813738a2
Ilja Sidoroff <ilja.sidoroff@iki.fi>
2019-09-15 13:07:43 +0300

    Chapter 5, Putting it together

---
+++ src/bin/rays.rs
@@ -0,0 +1,35 @@
+extern crate ray_tracer_challenge;
+
+use ray_tracer_challenge::canvas::Canvas;
+use ray_tracer_challenge::intersection;
+use ray_tracer_challenge::ray::Ray;
+use ray_tracer_challenge::sphere::Sphere;
+use ray_tracer_challenge::tuple::Tuple;
+
+fn main() {
+    let ray_origin = Tuple::point(0.0, 0.0, -5.0);
+    let wall_z = 10.0;
+    let wall_size = 7.0;
+    let canvas_pixels = 100;
+    let pixel_size = wall_size / (canvas_pixels as f64);
+    let half = wall_size / 2.0;
+
+    let mut canvas = Canvas::new(canvas_pixels, canvas_pixels);
+    let color = Tuple::color(1.0, 0.0, 0.0);
+    let shape = Sphere::new();
+
+    for y in 0..canvas_pixels {
+        let world_y = half - pixel_size * (y as f64);
+        for x in 0..canvas_pixels {
+            let world_x = -half + pixel_size * (x as f64);
+            let position = Tuple::point(world_x, world_y, wall_z);
+            let r = Ray::new(ray_origin, (position - ray_origin).normalize());
+            let xs = shape.intersect(&r);
+
+            if intersection::hit(&xs).is_some() {
+                canvas.write_pixel(x, y, color);
+            }
+        }
+    }
+    println!("{}", canvas.to_ppm());
+}
--- src/intersection.rs
+++ src/intersection.rs
@@ -2,7 +2,7 @@ use std::cmp::Ordering;
 
 use crate::sphere::Sphere;
 
-#[derive(Debug, PartialEq)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Intersection<'a> {
     pub t: f64,
     pub object: &'a Sphere,
@@ -17,13 +17,13 @@ impl<'a> Intersection<'a> {
     }
 }
 
-pub fn intersections<'a>(xs: &[&'a Intersection]) -> Vec<&'a Intersection<'a>> {
-    let mut v = xs.to_vec();
-    v.sort_by(|a, b| a.t.partial_cmp(&b.t).unwrap_or(Ordering::Equal));
-    v
+pub fn intersections<'a>(xs: &[Intersection<'a>]) -> Vec<Intersection<'a>> {
+    let mut ys = xs.to_owned();
+    ys.sort_by(|a, b| a.t.partial_cmp(&b.t).unwrap_or(Ordering::Equal));
+    ys.to_vec()
 }
 
-pub fn hit<'a>(xs: &'a Vec<&'a Intersection>) -> Option<&'a Intersection<'a>> {
+pub fn hit<'a>(xs: &'a [Intersection]) -> Option<&'a Intersection<'a>> {
     for x in xs {
         if x.t >= 0.0 {
             return Some(x);
@@ -51,7 +51,7 @@ mod tests {
         let s = Sphere::new();
         let i1 = Intersection::new(1.0, &s);
         let i2 = Intersection::new(2.0, &s);
-        let xs = intersections(&vec![&i1, &i2]);
+        let xs = intersections(&vec![i1, i2]);
 
         assert_eq!(2, xs.len());
         assert_eq!(1.0, xs[0].t);
@@ -63,7 +63,7 @@ mod tests {
         let s = Sphere::new();
         let i1 = Intersection::new(1.0, &s);
         let i2 = Intersection::new(2.0, &s);
-        let xs = intersections(&vec![&i1, &i2]);
+        let xs = intersections(&vec![i1, i2]);
         let i = hit(&xs);
 
         assert_eq!(Some(&i1), i);
@@ -74,7 +74,7 @@ mod tests {
         let s = Sphere::new();
         let i1 = Intersection::new(-1.0, &s);
         let i2 = Intersection::new(1.0, &s);
-        let xs = intersections(&vec![&i1, &i2]);
+        let xs = intersections(&vec![i1, i2]);
         let i = hit(&xs);
 
         assert_eq!(Some(&i2), i);
@@ -85,7 +85,7 @@ mod tests {
         let s = Sphere::new();
         let i1 = Intersection::new(-2.0, &s);
         let i2 = Intersection::new(-1.0, &s);
-        let xs = intersections(&vec![&i1, &i2]);
+        let xs = intersections(&vec![i1, i2]);
         let i = hit(&xs);
 
         assert_eq!(None, i);
@@ -98,7 +98,7 @@ mod tests {
         let i2 = Intersection::new(7.0, &s);
         let i3 = Intersection::new(-3.0, &s);
         let i4 = Intersection::new(2.0, &s);
-        let xs = intersections(&vec![&i1, &i2, &i3, &i4]);
+        let xs = intersections(&vec![i1, i2, i3, i4]);
         let i = hit(&xs);
 
         assert_eq!(Some(&i4), i);
